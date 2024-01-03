ece4745ded52f1504bef320e03525e19dbbcaf0b
Chaitya18 <chaityashah1819@gmail.com>
2024-01-03 21:42:55 +0530

    _

--- Module 3/q60/lib/main.dart
+++ Module 3/q60/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:q60/Screen/Second.dart';
+//import 'package:q60/Screen/Second.dart';
 import 'package:q60/Screen/first.dart';
 
 void main() {
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: My2ndScreen(),
+      home: Screen1(),
     );
   }
 }
