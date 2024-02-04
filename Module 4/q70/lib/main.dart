b9d659e5b41b79af421f4847c031853c78ebe4ea
Chaitya18 <chaityashah1819@gmail.com>
2024-02-04 14:43:17 +0530

    _

--- Module 4/q70/lib/main.dart
+++ Module 4/q70/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,15 +19,179 @@ class MyApp extends StatelessWidget {
 }
 
 class _Q70 extends StatefulWidget {
-  const _Q70({super.key});
+  _Q70({super.key});
 
   @override
   State<_Q70> createState() => __Q70State();
 }
 
 class __Q70State extends State<_Q70> {
+  List items = [GalleryPage(), AudioPage(), VideoPage()];
+  int currentindex = 0;
+  void tapClick(int index) {
+    setState(() {
+      currentindex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: items.elementAt(currentindex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.black,
+              ),
+              label: "Gallery"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.audio_file,
+                color: Colors.black,
+              ),
+              label: "Audio"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.video_call,
+                color: Colors.black,
+              ),
+              label: "Video"),
+        ],
+        type: BottomNavigationBarType.fixed,
+        iconSize: 24,
+        elevation: 5,
+        onTap: tapClick,
+        currentIndex: currentindex,
+      ),
+    );
+  }
+}
+
+//Gallery Page
+class GalleryPage extends StatelessWidget {
+  GalleryPage({super.key});
+  List images = ['1.jpeg', '2.jpeg', '3.jpeg', '4.jpeg', '5.jpeg', '6.jpeg'];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(images.length, (index) {
+          return Container(
+            height: 300,
+            width: double.maxFinite,
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                border: Border.all(width: 5),
+                borderRadius: BorderRadius.circular(25),
+                image: DecorationImage(
+                    image: AssetImage("images/${images[index]}"),
+                    fit: BoxFit.cover)),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+//Video Page
+class VideoPage extends StatelessWidget {
+  const VideoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Video"),
+    );
+  }
+}
+
+//Audio Page
+class AudioPage extends StatefulWidget {
+  const AudioPage({super.key});
+
+  @override
+  State<AudioPage> createState() => _AudioPageState();
+}
+
+class _AudioPageState extends State<AudioPage> {
+  late AudioPlayer music;
+  bool isPlaying = false;
+  dynamic paths = 'audio/kftn.mp3';
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: Text("Music"),
+          trailing: IconButton(
+            icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+            onPressed: () {
+              if (isPlaying) {
+                pauseAudio();
+              } else {
+                playAudio();
+              }
+            },
+          ),
+        ),
+        ListTile(
+          leading: Text("Music"),
+          trailing: IconButton(
+            icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+            onPressed: () {
+              if (isPlaying) {
+                pauseAudio();
+              } else {
+                playAudio();
+              }
+            },
+          ),
+        ),
+        ListTile(
+          leading: Text("Music"),
+          trailing: IconButton(
+            icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+            onPressed: () {
+              if (isPlaying) {
+                pauseAudio();
+              } else {
+                playAudio();
+              }
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  void playAudio() async {
+    dynamic result = music.play(paths);
+    if (result == 1) {
+      setState(() {
+        isPlaying = true;
+      });
+    }
+  }
+
+  void initState() {
+    super.initState();
+    music = AudioPlayer();
+  }
+
+  void dispose() {
+    music.dispose();
+    super.dispose();
+  }
+
+  void pauseAudio() async {
+    dynamic result = music.pause();
+    if (result == 1) {
+      isPlaying = false;
+    }
   }
 }
---
+++ Module 4/q71/android/app/src/main/kotlin/com/example/q71/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.q71
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
